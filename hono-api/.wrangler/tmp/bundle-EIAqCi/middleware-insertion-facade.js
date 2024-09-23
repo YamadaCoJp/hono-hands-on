a7ae211d09cda921607c6921eff3c1168a0eb81f
yamada_co_jp <f.atsuya.personal@gmail.com>
2024-09-24 02:33:53 +0900

    ok?

---
+++ hono-api/.wrangler/tmp/bundle-EIAqCi/middleware-insertion-facade.js
@@ -0,0 +1,11 @@
+				import worker, * as OTHER_EXPORTS from "C:\\Users\\atsuy\\work\\Hono-Hands-on\\hono-api\\src\\index.tsx";
+				import * as __MIDDLEWARE_0__ from "C:\\Users\\atsuy\\work\\Hono-Hands-on\\hono-api\\node_modules\\wrangler\\templates\\middleware\\middleware-ensure-req-body-drained.ts";
+import * as __MIDDLEWARE_1__ from "C:\\Users\\atsuy\\work\\Hono-Hands-on\\hono-api\\node_modules\\wrangler\\templates\\middleware\\middleware-miniflare3-json-error.ts";
+
+				export * from "C:\\Users\\atsuy\\work\\Hono-Hands-on\\hono-api\\src\\index.tsx";
+
+				export const __INTERNAL_WRANGLER_MIDDLEWARE__ = [
+					
+					__MIDDLEWARE_0__.default,__MIDDLEWARE_1__.default
+				]
+				export default worker;
